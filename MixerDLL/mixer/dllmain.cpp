d2849e41cd3e8358155668544bf652b351224ddd
lushuwen <lushuwen_gz@foxmail.com>
2015-06-03 09:59:51 +0800

    1: 增加系统判断

--- MixerDLL/mixer/dllmain.cpp
+++ MixerDLL/mixer/dllmain.cpp
@@ -1,32 +1,35 @@
 #pragma once
 
 #include <windows.h>
-
+#include <VersionHelpers.h>
 #include <stdio.h>
 #include <tchar.h>
 #include <windows.h>
 #include <atlbase.h>
 #include <atlstr.h>
 
-#include <conio.h> // getch()
+#include <conio.h> 
 #include <mmdeviceapi.h>
 #include <audiopolicy.h>
 #include <endpointvolume.h>
 
 #include "Psapi.h"  
 #pragma comment (lib,"Psapi.lib")  
+#pragma comment(lib, "Ntdll.lib")
 
 
 
-#define MIXER_DLL extern "C" __declspec(dllexport)
+#define MIXER_DLL extern "C" __declspec(dllexport) 
 
 CComPtr<IAudioEndpointVolume> pEpVol;					  // global volume
 CComPtr<IAudioSessionEnumerator> pAudioSessionEnumerator; // to go through sessions (local volume) 
 
+
 // filename
-static char VERSION[] = "2015-06-02-0044j";
-static char g_pidName[MAX_PATH] = { 0 };
-static char g_pidTitle[MAX_PATH] = { 0 };
+static const char VERSION[] = "2015-06-02-0044j";
+static char g_pidName[MAX_PATH + 1] = { 0 };
+static char g_pidTitle[MAX_PATH + 1] = { 0 };
+
 
 // ErrorMessages
 #define ERRMSG_OK "OK"
@@ -37,17 +40,18 @@ static char g_pidTitle[MAX_PATH] = { 0 };
 #define ERRMSG_SESSIONCONTROL2_INIT_FAILED "SessionControl2 Init failed"
 #define ERRMSG_AUDIOVOLUME_INIT_FAILED "ISimpleAudioVolume Init failed"
 #define ERRMSG_GETPROCESSINFO_FAILED "Get info about process failed"
+#define ERRMSG_SYSTEM_NOT_SUPORTED "Minimum supported client : vista "
 
 
 /// functions
 //	stuff
-MIXER_DLL char* version();
-MIXER_DLL void initAudio(); // call before using other audio functions!
+MIXER_DLL const char* version();
+MIXER_DLL bool initAudio(); // call before using other audio functions!
 MIXER_DLL void exitAudio(); // call before exit
 MIXER_DLL char* getErrorMessage();
 
 //	strings...
-MIXER_DLL int getSessionCount();
+MIXER_DLL int getSessionID();
 MIXER_DLL DWORD getSessionPID(const int sessionId);
 MIXER_DLL const char* getSessionName(const int sessionId);
 MIXER_DLL const char* getSessionTitle(const int sessionId);
@@ -65,15 +69,13 @@ MIXER_DLL float getSessionVolume(const int sessionId);
 MIXER_DLL bool setMasterVolume(const float volume);
 MIXER_DLL float getMasterVolume();
 MIXER_DLL void printAudioInfo();
-
-
 /// implementations
-
-char* version(){
+const char* version(){
 	return VERSION;
 }
 
-void initAudio(){
+bool initAudio(){
+	if (!IsWindowsVistaOrGreater()) return false;
 	CComPtr<IMMDevice> pDevice; // get Mgr & EndpointVolume
 	CComPtr<IMMDeviceEnumerator> pDeviceEnumerator; // select output device
 	CComPtr<IAudioSessionManager2> pAudioSessionManager2; // get enumerator
@@ -89,13 +91,15 @@ void initAudio(){
 	pDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, NULL, (VOID**)&pEpVol);
 	// get Audio Session Enumerator
 	pAudioSessionManager2->GetSessionEnumerator(&pAudioSessionEnumerator);
+
+	return true;
 }
 
 char* errorMessage = NULL;
 char* getErrorMessage(){
 	return errorMessage;
 }
-int getSessionCount(){
+int getSessionID(){
 	int sessionCount = 0;
 	exitAudio();
 	initAudio();
@@ -107,7 +111,7 @@ int getSessionCount(){
  * Find out if a session is muted. -1 -> Master
  */
 bool getSessionMute(const int sessionId){
-	int sessionCount = getSessionCount();
+	int sessionCount = getSessionID();
 	if (sessionId == -1)
 		return getMasterMute();
 	if (sessionId >= sessionCount || sessionId < 0) {
@@ -135,7 +139,7 @@ bool getSessionMute(const int sessionId){
  Mutes/Unmutes a session. If sessionId=-1, info about master
  */
 bool setSessionMute(const int sessionId, const bool mute){
-	int sessionCount = getSessionCount();
+	int sessionCount = getSessionID();
 	if (sessionId == -1)
 		return setMasterMute(mute);
 	if (sessionId >= sessionCount || sessionId < 0) {
@@ -163,7 +167,7 @@ bool setSessionMute(const int sessionId, const bool mute){
  Sets volume of sessionId. If sessionId=-1, sets Master Volume
  */
 bool setSessionVolume(const int sessionId, const float volume){
-	int sessionCount = getSessionCount();
+	int sessionCount = getSessionID();
 	if (sessionId == -1)
 		return setMasterVolume(volume);
 	if (sessionId >= sessionCount || sessionId < 0) {
@@ -196,7 +200,7 @@ bool setSessionVolume(const int sessionId, const float volume){
 float getSessionVolume(const int sessionId){
 	CComPtr<IAudioSessionControl> pSessionControl;
 	CComPtr<ISimpleAudioVolume> pSAV;
-	int sessionCount = getSessionCount();
+	int sessionCount = getSessionID();
 	float vol; 
 	if (sessionId == -1)
 		return getMasterVolume();
@@ -228,7 +232,7 @@ float getSessionVolume(const int sessionId){
 DWORD getSessionPID(const int sessionId){
 	CComPtr<IAudioSessionControl> pSessionControl;
 	CComPtr<IAudioSessionControl2> pSC2;
-	int sessionCount = getSessionCount();
+	int sessionCount = getSessionID();
 	if (sessionId >= sessionCount || sessionId < 0) { 
 		errorMessage = ERRMSG_UNDEFINEDSESSIONID;
 		return NULL;
@@ -297,12 +301,13 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
 }
 
 const char* getPIDTitle (DWORD procid){
+	memset(g_pidTitle, 0, MAX_PATH);
 	EnumWindows(EnumWindowsProc, (LPARAM)&procid);
 	return g_pidTitle;
 }
 
 void printAudioInfo(){
-	int sessionCount = getSessionCount();
+	int sessionCount = getSessionID();
 	printf("\n\n\n\n\n\n\n\n");
 	printf("################################################################################");
 	printf("     Audio Sessions \n");
